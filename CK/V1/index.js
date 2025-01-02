fa3246ddd8c28195179cf7d433080d87939e0348
Michael Swain <michaelswain@tommy.com>
2025-01-02 13:45:18 +0000

    Add translations for USP copy

--- CK/V1/index.js
+++ CK/V1/index.js
@@ -1,15 +1,25 @@
 const CX421 = {
+    copy: {
+        gb: '60-day free returns',
+        nl: '60 dagen gratis retourneren',
+        de: '60 Tage kostenlose Rückgabe',
+        fr: 'Retours gratuits sous 60 jours',
+        it: 'Resi gratuiti entro 60 giorni',
+        es: '60 días para devoluciones',
+        pl: 'Darmowe zwroty w ciągu 60 dni',
+        default: '60-day free returns'
+    },
     createUSPelem: () => {
         const elem = document.createElement('div');
+        const text = CX421.copy[window.__NEXT_DATA__.props.pageProps.initialState.currentStore.target] || CX421.copy.default;
         elem.innerHTML = `<ul>
-                            <li>60-days free returns</li>
+                            <li>${text}</li>
                         </ul>`;
         return elem;
     },
     init: () => {
         const USPelem = CX421.createUSPelem();
         optimizely.utils.observeSelector('[data-testid="proceed-to-payment-pvh-button"], [data-testid="place-order-pvh-button"]', target => {
-            console.log("CTA found");
             target.parentElement.insertBefore(USPelem, target.nextElementSibling);
         })
     }
--- TH/V2/index.js
+++ TH/V2/index.js
@@ -1,15 +1,25 @@
 const CX421 = {
+    copy: {
+        gb: '60-day free returns',
+        nl: 'Gratis retourneren 60 dagen',
+        de: '60 Tage kostenloses Rückgaberecht',
+        fr: 'Retours gratuits pendant 60 jours',
+        it: 'Reso gratuito entro 60 giorni',
+        es: 'Devoluciones gratuitas de 60 días',
+        pl: '60 dni na bezpłatny zwrot',
+        default: '60-day free returns'
+    },
     createUSPelem: () => {
         const elem = document.createElement('div');
+        const text = CX421.copy[window.__NEXT_DATA__.props.pageProps.initialState.currentStore.target] || CX421.copy.default;
         elem.innerHTML = `<ul>
-                            <li>60-days free returns</li>
+                            <li>${text}</li>
                         </ul>`;
         return elem;
     },
     init: () => {
         const USPelem = CX421.createUSPelem();
         optimizely.utils.observeSelector('[data-testid="basket-overview-total-list-listItem"]', target => {
-            console.log("CTA found");
             target.parentElement.insertBefore(USPelem, target.nextElementSibling);
         })
     }
